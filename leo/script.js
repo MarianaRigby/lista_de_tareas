1a0856c888da6a16537e335fd1cf0eaa01681c34
MarianaRigby <leonardorigby@Leonardos-MacBook-Air-2.local>
2025-03-18 18:52:14 -0600

    mi perfil

--- leo/script.js
+++ leo/script.js
@@ -6,7 +6,12 @@ const arrayDePrueba = ["rojo", "verde", "azul"];
 let htmlDeBotones = `<button class="bg-black text-white h-10 px-5 rounded">Boton de inicio</button>`;
 
 arrayDePrueba.forEach((valor, indice) => {
-  htmlDeBotones += `<button class="block my-4 bg-blue-600 text-white h-10 px-5 rounded">Boton ${valor}</button>`;
+  if (valor === "rojo") {
+    htmlDeBotones += `<button class="block my-4 bg-red-700 text-white h-10 px-5 rounded">Boton ${valor}</button>`;
+  } else if (valor === "verde") {
+    htmlDeBotones += `<button class="block my-4 bg-green-700 text-white h-10 px-5 rounded">Boton ${valor}</button>`;
+  } else {
+    htmlDeBotones += `<button class="block my-4 bg-blue-700 text-white h-10 px-5 rounded">Boton ${valor}</button>`;
+  }
 });
-
 root.innerHTML = htmlDeBotones;
---
+++ perfil/script.js
@@ -0,0 +1,63 @@
+const miPerfil = {
+  nombreApellidos: "Mariana Rigby Castillo",
+  edad: 20,
+  estaActivo: true,
+  imagen:
+    "https://orquideafloral.com.mx/wp-content/uploads/2024/06/89f722edf28550b553d2faa3e83d48f2-844x1500.webp",
+  correo: "marianarigby10@gmail.com",
+  celular: 3313317542,
+  coloresFavoritos: ["azul", "rosita", "morado", "amarillo", "verde"],
+};
+let colorEstado;
+let textoEstado;
+const perfilContainer = document.getElementById("perfilContainer");
+
+if (miPerfil.estaActivo === true) {
+  colorEstado = "bg-green-500";
+  textoEstado = "Activo";
+} else {
+  colorEstado = "bg-red-500";
+  textoEstado = "Inactivo";
+}
+let html = `
+    <img src="${miPerfil.imagen}" class="w-[250px] h-[250px] mx-auto rounded-full">
+      
+    <div class="flex items-center justify-center mt-2">
+        <div class="w-3 h-3 ${colorEstado} rounded-full mr-2"></div>
+        <p class="text-gray-700 font-semibold">${textoEstado}</p>
+    </div>
+    
+    
+     
+
+    <h2 class="text-xl  font-medium font-serif text-sky-950 mt-3">Nombre: ${miPerfil.nombreApellidos}</h2>
+
+    <h3 class="font-serif font-semibold text-center text-2xl mt-4">Datos del contacto</h3>
+    <hr class=" border border-sky-800">
+    <p class="font-serif font-medium text-xl text-sky-950 mb-2">Edad: ${miPerfil.edad} años</p>
+    <p class="font-serif   font-medium text-xl text-sky-950 mb-2">Correo: ${miPerfil.correo}</p>
+    <p class="font-serif font-medium text-xl text-sky-950 mb-2">Celular: ${miPerfil.celular}</p>
+  
+    <p class="font-serif font-medium text-xl text-sky-950 mb-2">Colores Favoritos:</p>
+    
+
+`;
+
+miPerfil.coloresFavoritos.forEach((color) => {
+  if (color.toLocaleLowerCase() === "azul") {
+    colorClase = "bg-blue-500";
+  } else if (color.toLowerCase() === "rosita") {
+    colorClase = "bg-pink-500";
+  } else if (color.toLowerCase() === "morado") {
+    colorClase = "bg-purple-500";
+  } else if (color.toLowerCase() === "amarillo") {
+    colorClase = "bg-yellow-500";
+  } else if (color.toLowerCase() === "verde") {
+    colorClase = "bg-green-500";
+  }
+
+  html += `<div class="inline-flex   w-6 h-6 ${colorClase} rounded-full m-1"></div>`;
+});
+
+perfilContainer.innerHTML = html;
+html += `<p>mmkedmekdmek</p>`;
