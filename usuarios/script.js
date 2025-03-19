822a4f898b0cf807cb6ce474b4e7ccc220ae069b
lrcdeveloper <leorigbyc@gmail.com>
2025-03-18 19:39:02 -0600

    feat: users array

--- usuarios/script.js
+++ usuarios/script.js
@@ -1,1 +1,10 @@
-const root = document.getElementById("root");
+const root = document.getElementById("users-container");
+const users = [
+  {
+    name: "Emma",
+    lastName: "Dorsey",
+    role: "Senior Developer",
+    photoURL:
+      "https://images.unsplash.com/photo-1505840717430-882ce147ef2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
+  },
+];
