77d9cc2f4ba2037dc63be2584d29332691bdfd19
lrcdeveloper <leorigbyc@gmail.com>
2025-03-21 16:58:32 -0600

    estructuras de control

---
+++ estructuras-de-control/index.js
@@ -0,0 +1,20 @@
+const user = {
+  name: "Mariana",
+  lastName: "Rigby Castillo",
+  edad: 20,
+  monthOfBirth: 11,
+  favoriteColors: ["green", "purple", "blue"],
+};
+
+// Ejercicio con Condicionales (if-else) -----------------
+// Validar e imprimir si el usuario:
+// - es menor de edad
+// - mayor de edad o
+// - adulto mayor
+
+// Ejercicio con (switch-case)
+// Validar e imprime el nombre del mes de nacimiento del usuario: -----------------
+
+//Ejercicio con Bucle (for) -----------------
+// Muestra en la consola la tabla de multiplicar de un numero dado
+const numero = 2;
